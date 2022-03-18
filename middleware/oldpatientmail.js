fbd4105f318ce615622c43ee27a23221adf534cd
Kumari-Ujwala-Sinha <kumariujwalas@gmail.com>
2022-03-18 09:31:37 +0530

    dratef

--- middleware/oldpatientmail.js
+++ middleware/oldpatientmail.js
@@ -31,7 +31,7 @@ const oldpatientmail =async (req, res, next) => {
       to: [`${email}`],
       subject: `${name} Booked Succefully`,
       html: `<div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
-      <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to the AL-OSRAH INTERNATIONAL HOSPITAL</h2>
+      <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to the Dr. Atefbassas HOSPITAL</h2>
       <div><div style="font-size: 14px margin">
       <div style="padding : 4px 0px 0px 0px"><h3> Name: </h3>${name}</div>
       <div style="padding : 4px 0px 0px 0px"><h3> File Number: </h3>${fileid}</div>
